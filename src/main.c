6ff01b5d527df93345e58c673e146202f38c9bbb
clach04 <Chris.Clark@actian.com>
2015-11-29 22:26:02 -0800

    Larger APNG added along with workaround code
    Based on https://github.com/pebble-hacks/gbitmap-sequence-example/blob/master/src/gbitmap-sequence.c calling load_sequence() instead of gbitmap_sequence_restart().

--- src/main.c
+++ src/main.c
@@ -15,20 +15,38 @@ static GBitmap *s_bitmap = NULL;
 static BitmapLayer *s_bitmap_layer;
 static GBitmapSequence *s_sequence = NULL;
 
+static void load_sequence();
+
 static void timer_handler(void *context) {
   uint32_t next_delay;
 
+APP_LOG(APP_LOG_LEVEL_DEBUG, "timer_handler() ENTRY");
   // Advance to the next APNG frame
   if(gbitmap_sequence_update_bitmap_next_frame(s_sequence, s_bitmap, &next_delay)) {
+APP_LOG(APP_LOG_LEVEL_DEBUG, "in if; pre bitmap_layer_set_bitmap()");
     bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+APP_LOG(APP_LOG_LEVEL_DEBUG, "in if; pre layer_mark_dirty()");
     layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
 
     // Timer for that delay
+APP_LOG(APP_LOG_LEVEL_DEBUG, "in if; about to call timer reg");
     app_timer_register(next_delay, timer_handler, NULL);
   } else {
+APP_LOG(APP_LOG_LEVEL_DEBUG, "in else");
     // Start again
+      /*
+      ** Docs say restart, but this does NOT restart after:
+      gbitmap_sequence_update_bitmap_next_frame() results in error trace
+[ERROR] tmap_sequence.c:219: APNG memory allocation failed
+[ERROR] tmap_sequence.c:336: gbitmap_sequence failed to update bitmap
+
+from https://github.com/pebble-hacks/gbitmap-sequence-example/blob/master/src/gbitmap-sequence.c
+call load_sequence(); instead
     gbitmap_sequence_restart(s_sequence);
+    */
+      load_sequence();
   }
+APP_LOG(APP_LOG_LEVEL_DEBUG, "timer_handler() EXIT");
 }
 
 static void load_sequence() {
@@ -42,10 +60,12 @@ static void load_sequence() {
     s_bitmap = NULL;
   }
 
+APP_LOG(APP_LOG_LEVEL_DEBUG, "pre gbitmap_sequence_create_with_resource()");
   // Create sequence
   s_sequence = gbitmap_sequence_create_with_resource(RESOURCE_ID_ANIMATION);
 
   // Create GBitmap
+APP_LOG(APP_LOG_LEVEL_DEBUG, "pre gbitmap_create_blank()");
   s_bitmap = gbitmap_create_blank(gbitmap_sequence_get_bitmap_size(s_sequence), GBitmapFormat8Bit);
 
   // Begin animation
